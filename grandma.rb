2e2f583c3a95823d74975e055272d109291d0e61
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-07 19:42:57 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     puts 'I LOVE YOU TOO PUMPKIN!'
-  elsif string.upcase == true
+  elsif string.upcase
     puts 'NO, NOT SINCE 1938!'
   else
     puts 'HUH?! SPEAK UP, SONNY!'
